2ca0683e803c16a51071c4ef379791bca8f78547
Scott Prahl <scott@prahls.com>
2018-03-05 12:04:15 -0800

    update version

--- ofiber/__init__.py
+++ ofiber/__init__.py
@@ -3,16 +3,14 @@
 ofiber: Design and Analysis of Optical Fibers
 ================================================================================
 
-This code was originally published by 
-    Copyright (C) 2018 - Scott Prahl
-    
     http://github.com/scottprahl/ofiber
 """
 
 __author__ = 'Scott Prahl'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 from ofiber.refraction import *
 from ofiber.dispersion import *
 from ofiber.cylinder_step import *
 from ofiber.planar_step import *
+from ofiber.noise import *
--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 """
-Copyright 2017 Scott Prahl
+Copyright 2018 Scott Prahl
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
@@ -13,7 +13,7 @@ from setuptools import setup
 setup(
 	name='ofiber',
 	packages=['ofiber'],
-	version='0.1.0',
+	version='0.2.0',
 	description='Light Propagation in Optical Fibers',
 	url='https://github.com/scottprahl/ofiber.git',  
 	author='Scott Prahl',
