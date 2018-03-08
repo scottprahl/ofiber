9bd7848965b65b96d940903c06aae148cdbb78a2
Scott Prahl <scott@prahls.com>
2018-03-07 20:54:33 -0800

    fix pylint errors

--- ofiber/noise.py
+++ ofiber/noise.py
@@ -1,20 +1,28 @@
+"""
+Useful routines for noise in optical communications
+
+Based on chapter 13 of A. Ghatak, K. Thyagarajan, An Introduction to
+Fiber Optics, Cambridge University Press, 1998
+
+ToDo:
+    * Add equation numbers to functions
+"""
+
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy.special import erfc
 from scipy.special import erfcinv
 
-__all__ = [ 'shot_noise',
-            'thermal_noise',
-            'NEP',
-            'best_APD_gain',
-            'BER_at_SNR',
-            'SNR_at_BER',
-            'thermal_min_power',
-            'quantum_min_power',
-            ]
+__all__ = ['shot_noise',
+           'thermal_noise',
+           'NEP',
+           'best_APD_gain',
+           'BER_at_SNR',
+           'SNR_at_BER',
+           'thermal_min_power',
+           'quantum_min_power']
 
 
-def shot_noise(I,Idark,bandwidth, M=1, x=0):
+def shot_noise(I, Idark, bandwidth, M=1, x=0):
     """
     Return the noise current associated with shot/poisson noise
     Inputs:
@@ -25,11 +33,11 @@ def shot_noise(I,Idark,bandwidth, M=1, x=0):
     Returns:
         shot_noise       (A)
     """
-    q = 1.602e-19 #coulomb
-    return np.sqrt(2*q*(I/M+Idark)*bandwidth*M**(2+x))
+    q = 1.602e-19  # Coulomb
+    return np.sqrt(2 * q * (I / M + Idark) * bandwidth * M**(2 + x))
 
 
-def thermal_noise(T,Rload,bandwidth):
+def thermal_noise(T, Rload, bandwidth):
     """
     Return the noise current associated with thermal processes
     Inputs:
@@ -39,11 +47,11 @@ def thermal_noise(T,Rload,bandwidth):
     Returns:
         thermal_noise       (A)
     """
-    k = 1.38e-23   #J/K
-    return np.sqrt(4*k*T*bandwidth/Rload)
+    k = 1.38e-23  # J/K
+    return np.sqrt(4 * k * T * bandwidth / Rload)
 
 
-def NEP(Responsivity,Rload,Idark,T):
+def NEP(Responsivity, Rload, Idark, T):
     """
     Return noise equivalent power
     Inputs:
@@ -54,15 +62,26 @@ def NEP(Responsivity,Rload,Idark,T):
     Returns:
         power       (W/sqrt(Hz))
     """
-    q = 1.602e-19 #coulomb
-    k = 1.38e-23   #J/K
-    return 1/Responsivity*np.sqrt(4*k*T/Rload + 2*q*Idark)
-
-
-def best_APD_gain(I,Rload,Idark,x,T):
-    q = 1.602e-19  # coulomb
+    q = 1.602e-19  # Coulomb
     k = 1.38e-23   # J/K
-    return (4*k*T/(x*q*Rload*(I+Idark)))**(1/(x+2))
+    return 1 / Responsivity * np.sqrt(4 * k * T / Rload + 2 * q * Idark)
+
+
+def best_APD_gain(I, Rload, Idark, x, T):
+    """
+    Return best gain for an avalanche photodiode
+    Inputs:
+        I     = current         (A)
+        Idark = dark current    (A)
+        Rload = load resistance (Ohms)
+        x     = excess noise (0.3 for Si, 0.7 for InGaAs, 1.0 for Ge APDs)
+        T     = Temperature
+    Returns:
+        optimal gain (--)
+    """
+    q = 1.602e-19  # Coulomb
+    k = 1.38e-23   # J/K
+    return (4 * k * T / (x * q * Rload * (I + Idark)))**(1 / (x + 2))
 
 
 def BER_at_SNR(snr):
@@ -73,7 +92,7 @@ def BER_at_SNR(snr):
     Returns:
         bit error rate              (--)
     """
-    return 0.5*erfc(np.sqrt(snr/8))
+    return 0.5 * erfc(np.sqrt(snr / 8))
 
 
 def SNR_at_BER(ber):
@@ -84,10 +103,10 @@ def SNR_at_BER(ber):
     Returns:
         signal to noise ratio (--)
     """
-    return 8*erfcinv(2*ber)**2
+    return 8 * erfcinv(2 * ber)**2
 
 
-def thermal_min_power(bitrate,responsivity,capacitance,T,snr):
+def thermal_min_power(bitrate, responsivity, capacitance, T, snr):
     """
     Return the minimum optical power needed to achieve a signal-to-noise
     ratio assuming that thermal noise dominates
@@ -100,9 +119,9 @@ def thermal_min_power(bitrate,responsivity,capacitance,T,snr):
     Returns:
         optical power                            (W)
     """
-    k = 1.38e-23   #J/K
-    val = 2*np.pi*k*T*capacitance*snr
-    return bitrate/responsivity*np.sqrt(val)
+    k = 1.38e-23  # J/K
+    val = 2 * np.pi * k * T * capacitance * snr
+    return bitrate / responsivity * np.sqrt(val)
 
 
 def quantum_min_power(bitrate, ber, lambda0):
@@ -115,8 +134,8 @@ def quantum_min_power(bitrate, ber, lambda0):
     Returns:
         optical power                   (W)
     """
-    h = 6.626e-34  # J*s
-    c = 2.998e8    # m/s
-    nu = c/lambda0 # Hz
-    Np = -np.log(2*ber)
-    return h*nu*Np*bitrate
+    h = 6.626e-34     # J*s
+    c = 2.998e8       # m/s
+    nu = c / lambda0  # Hz
+    Np = -np.log(2 * ber)
+    return h * nu * Np * bitrate
