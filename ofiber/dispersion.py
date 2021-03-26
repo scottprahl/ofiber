c1dcb26ef3785bfc26d806520da0080af543dafb
Scott Prahl <scott@prahls.com>
2021-03-25 17:18:06 -0700

    streamline API

--- ofiber/dispersion.py
+++ ofiber/dispersion.py
@@ -1,6 +1,6 @@
 # pylint: disable=invalid-name
 # pylint: disable=too-many-arguments
-
+# pylint: disable=non-ascii-name
 """
 Useful routines for dispersion in cylindrical waveguides.
 
@@ -19,115 +19,83 @@ import ofiber.basics as ofb
 
 __all__ = ('Material_Dispersion',
            'Waveguide_Dispersion',
-           'Waveguide_Dispersion_Approx',
-           'Waveguide_Dispersion_Delta',
-           'Total_Dispersion')
+           'Dispersion',
+           )
 
-
-def Material_Dispersion(glass, lambda0):
+def Material_Dispersion(core, λ):
     """
     Calculate the material dispersion using Sellmeier coefficients.
 
     Args:
-        glass  : an array of Sellmeier coefficients.
-        lambda0: wavelength [m]
+        core: array of Sellmeier coefficients for core
+        λ:    wavelength [m]
 
     Returns:
         material dispersion [s/m**2]   (multiply by 1e6 to get ps/(km*nm))
     """
     c = scipy.constants.speed_of_light
-    return -lambda0 * ofr.d2n(glass, lambda0) / c  # s/m**2
+    return -λ * ofr.d2n(core, λ) / c
 
 
-def Waveguide_Dispersion(n1, n2, a, lambda0, q=1e20):
+def Waveguide_Dispersion(n_core, n_clad, r_core, λ, q=1e20, approx=False):
     """
-    Calculate the waveguide dispersion of a step index fiber.
+    Calculate the waveguide dispersion of a fiber.
+    
+    The default value of q represents a step index fiber.  Other values
+    allow parabolic (q=2) or triangular (q=1) profiles.
 
     The waveguide dispersion is for the fundamental mode of the fiber.
+    
+    The approximation is reasonably good for values from 1.6<V<2.6 
 
     Args:
-        n1:      core index of refraction     [--]
-        n2:      cladding index of refraction [--]
-        a:       radius of the fiber          [m]
-        lambda0: wavelength                   [m]
+        n_core: core index of refraction          [-]
+        n_clad: cladding index of refraction      [-]
+        r_core: radius of the fiber core          [m]
+        λ:      wavelength in vacuum              [m]
+        q:      power in graded index fiber       [-]
+        approx: approximate when True             [True/False]
 
     Returns:
         waveguide dispersion [s/m**2]   (multiply by 1e6 to get ps/(km*nm))
     """
-    Delta = (n1**2 - n2**2) / 2 / n1**2
-    V = 2 * np.pi / lambda0 * a * np.sqrt(n1**2 - n2**2)
     c = scipy.constants.speed_of_light
-    esi_Delta = ofb.esi_Delta(Delta, q)
+    Δ = (n_core**2 - n_clad**2) / 2 / n_core**2
+    V = 2 * np.pi / λ * r_core * np.sqrt(n_core**2 - n_clad**2)
+
+    # Find the equivalent step index fiber parameters
+    esi_Delta = ofb.esi_Delta(Δ, q)
     esi_V = ofb.esi_V_parameter(V, q)
-    dw = -n2 * esi_Delta / c / lambda0 * ofc.V_d2bV_by_V(esi_V, 0)
-    return dw
 
-
-def Waveguide_Dispersion_Approx(n1, n2, a, lambda0, q=1e20):
-    """
-    Approximate the waveguide dispersion of the a single mode fiber.
-
-    Args:
-        n1:      index of core        [-]
-        n2:      index of cladding    [-]
-        a:       radius of fiber      [m]
-        lambda0: wavelength in vacuum [m]
-
-    Returns:
-        waveguide dispersion [s/m**2]   (multiply by 1e6 to get [ps/km/nm])
-    """
-    Delta = (n1**2 - n2**2) / 2 / n1**2
-    V = 2 * np.pi / lambda0 * a * np.sqrt(n1**2 - n2**2)
-    esi_Delta = ofb.esi_Delta(Delta, q)
-    esi_V = ofb.esi_V_parameter(V, q)
-    c = scipy.constants.speed_of_light
-    dw = -n2 * esi_Delta / c / lambda0 * ofc.V_d2bV_by_V_Approx(esi_V)
-    return dw
-
-
-def Waveguide_Dispersion_Delta(glass, Delta, a, lambda0, q=1e20, approx=False):
-    """
-    Calculate the waveguide dispersion of a glass optical fiber.
-
-    This is a convenience routine that finds the waveguide dispersion for
-    a specific type of core glass and refractive index difference.
-
-    Args:
-        glass:   array of Sellmeier coefficients [various]
-        Delta:   refractive index difference     [-]
-        a:       radius of fiber                 [m]
-        lambda0: wavelength in vacuum            [m]
-        q:       power in graded index fiber     [-]
-        approx:  pass True if appoximation is ok
-
-    Returns:
-        waveguide dispersion [s/m**2]   (multiply by 1e6 to get [ps/km/nm])
-    """
-    n1 = ofr.n(glass, lambda0)
-    n2 = n1 * (1 - Delta)
     if approx:
-        return Waveguide_Dispersion_Approx(n1, n2, a, lambda0, q)
+        vtemp = ofc.V_d2bV_by_V_Approx(esi_V)
+    else:
+        vtemp = ofc.V_d2bV_by_V(esi_V, 0)
+    return -n_clad * esi_Delta / c / λ * vtemp
 
-    return Waveguide_Dispersion(n1, n2, a, lambda0, q)
 
-
-def Total_Dispersion(glass, Delta, a, lambda0, q=1e20, approx=False):
+def Dispersion(core, n_clad, r_core, λ, q=1e20, approx=False):
     """
-    Calculate the total dispersion in an optical fiber.
+    Calculate the material and waveguide dispersion.
 
     This is a convenience routine that finds the total dispersion for
     a specific type of core glass and refractive index difference.
 
+    The returned dispersion is in units of [s/m**2].  To convert to
+    [ps/km/nm], multiply by 1e6.
+
     Args:
-        glass:   array of Sellmeier coefficients [various]
-        Delta:   refractive index difference     [-]
-        a:       radius of fiber                 [m]
-        lambda0: wavelength in vacuum            [m]
+        core:   Sellmeier coefficients for core   [various]
+        n_clad: index of cladding                 [-]
+        r_core: radius of fiber core              [m]
+        λ:      wavelength in vacuum              [m]
+        q:      power in graded index fiber       [-]
+        approx: approximate when True             [True/False]
 
     Returns:
-        waveguide dispersion [s/m**2]   (multiply by 1e6 to get [ps/km/nm])
+        material, waveguide, and total dispersion [s/m**2] 
     """
-    Dm = Material_Dispersion(glass, lambda0)                   # [s/m**2]
-    Dw = Waveguide_Dispersion_Delta(
-        glass, Delta, a, lambda0, q, approx)  # [s/m**2]
-    return Dw + Dm
+    n_core = ofr.n(core, λ)
+    Dm = Material_Dispersion(core, λ)
+    Dw = Waveguide_Dispersion(n_core, n_clad, r_core, λ, q, approx)
+    return Dm, Dw
