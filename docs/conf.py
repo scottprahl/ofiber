2bb74d84f86e4b035430fe5ccc81ae007f6db7f9
Scott Prahl <scott@prahls.com>
2022-03-13 12:59:09 -0700

    fix merge glitch

--- docs/conf.py
+++ docs/conf.py
@@ -17,11 +17,6 @@ import re
 import os.path
 
 project = 'ofiber'
-<<<<<<< HEAD
-copyright = '2018-2022 Scott Prahl'
-author = 'Scott Prahl'
-=======
->>>>>>> bf58f3a (put __version__ in __init__.py)
 
 def get_init_property(prop):
     """Return property from __init__.py."""
@@ -39,9 +34,7 @@ master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+# Sphinx extension module names
 extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
@@ -52,21 +45,10 @@ extensions = [
 numpydoc_show_class_members = False
 napoleon_use_rtype = False
 
-<<<<<<< HEAD
-# Add any paths that contain templates here, relative to this directory.
-# templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.tox', '**.ipynb_checkpoints',
-                    'sources/*', 'first_version/*', '8-Optical-Fiber-Amplifiers.ipynb']
-=======
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', '.DS_Store', '.tox', '**.ipynb_checkpoints']
->>>>>>> bf58f3a (put __version__ in __init__.py)
 
 # I execute the notebooks manually in advance. If notebooks test the code,
 # they should be run at build time.
@@ -77,20 +59,6 @@ nbsphinx_allow_errors = True
 source_suffix = ['.rst', '.ipynb']
 
 # -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = 'sphinx_rtd_theme'
-
-<<<<<<< HEAD
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
-
-=======
->>>>>>> bf58f3a (put __version__ in __init__.py)
 html_scaled_image_link = False
-
 html_sourcelink_suffix = ''
